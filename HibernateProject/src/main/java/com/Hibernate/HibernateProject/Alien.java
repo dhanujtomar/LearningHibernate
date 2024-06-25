31d552062f8dc2d6ed2f1dd75d6ae03a67d1269c
dhanujtomar <dhanujtomar@gmail.com>
2024-06-25 21:02:19 +0530

    first Commit

---
+++ HibernateProject/src/main/java/com/Hibernate/HibernateProject/Alien.java
@@ -0,0 +1,34 @@
+package com.Hibernate.HibernateProject;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name="alien_table")
+public class Alien {
+	@Id
+	private int aid;
+	private String name;
+	private String color;
+	
+	
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+}
